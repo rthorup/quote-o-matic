19e2d8a342e815edaec9bd75eb57df4c73e09c3f
r766t128 <rthorup@ku.edu>
2022-01-06 19:25:36 -0600

    ignore my lint please

--- src/components/IndividualQuote.js
+++ src/components/IndividualQuote.js
@@ -8,12 +8,14 @@ import ImageAndQuote from './ImageAndQuote.js';
 
 
 const IndividualQuote = (props) => {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   const charName = props.character;
   const [character, setCharacter] = useState("");
   const [quote, setQuote] = useState("");
   const [image, setImage] = useState("");
 
-  useEffect(() => {
+  useEffect((arr = props.character) => {
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     axios.get(`https://futuramaapi.herokuapp.com/api/characters/${charName}/1`)
     .then((res) => {
       const {character, image, quote} = res.data[0];
@@ -22,7 +24,7 @@ const IndividualQuote = (props) => {
       setImage(image);
 
     })
-  }, []);
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
   const getNewQuoteByCharacter = (character) => {
     character = character.character.split(/[\s]+/).join("-");
